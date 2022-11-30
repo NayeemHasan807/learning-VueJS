671d3dfd226b38373528510f784aa394bab959ca
NayeemHasan807 <moonnayeem87@gmail.com>
2022-11-30 22:37:40 +0600

    click and mousemove event useing V-on and event parameter of js

---
+++ Code-1/app.js
@@ -0,0 +1,19 @@
+/*Here we will see Event Use: V-on builtin directives on button element*/
+new Vue({
+    el: "#button",
+    data: {
+        name: "Nayeem",
+        x: 0,
+        y: 0
+    },
+    methods: {
+      updateName() {
+        this.name="Nayeem Hasan Moon"
+      },//main data will not be changed just the change will be shown on the display.
+      getCoord(event){
+        this.x = event.clientX;
+        this.y = event.clientY;
+      },//event parameter can give clints mouse pointer axix
+    },
+
+});
