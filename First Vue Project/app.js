58e54412b424a0574a8abe69c697e7e802e8fd8e
NayeemHasan807 <moonnayeem87@gmail.com>
2022-11-30 12:55:05 +0600

    use of builtin Directives: v-bind

--- First Vue Project/app.js
+++ First Vue Project/app.js
@@ -4,7 +4,7 @@
 new Vue({
     el: "#title", //el is basically the elemant. Here we used the div elemant in home.html which has id named title. 
     data: {
-        content: "Here we will learn how to use EL & Data Property:" //String value
+        content: "Here we will learn how to use Vue JS:" //String value
     } //we will use this data in home.html page on div elemant containing the id titel.
 });
 
@@ -37,5 +37,20 @@ new Vue({
         getname(){
             return this.name //by using this keyword we can send any value inside the data property.
         }
+    }
+});
+
+/* Now we will see the works of directives
+there are two types of directives one is built-in and another is custom
+here we will see V-bind builtin directives on image element*/
+
+new Vue({
+    el: "#imageandhref",
+    data: {
+        src: "https://media-exp1.licdn.com/dms/image/D5603AQFf4IcLjPqeAA/profile-displayphoto-shrink_800_800/0/1667525364059?e=1675296000&v=beta&t=5U6Ez2rObCYekfLZSkt_IYg4H3NyNzYj5OY_ZBmJMiQ",
+        alt: "This is me",
+        heightandwidth: "300",
+        linkedinlink: "https://www.linkedin.com/in/nayeemhasan807/",
+        click: "CLick the link to visit my Linkedin profile"
     }
 });
