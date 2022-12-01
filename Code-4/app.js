c0dda5018c785ad351814307e66dd97b2e9f402a
NayeemHasan807 <moonnayeem87@gmail.com>
2022-12-01 15:33:48 +0600

    using setTimeOut, mount method and template property to make a text dynamically appear after few moments

--- Code-4/app.js
+++ Code-4/app.js
@@ -1,5 +1,8 @@
-/*Creating multiple instance and accesing one instance from another one*/
-//To get an access of an instance we have to keep that instance in a veriable
+/*Creating multiple instance and accesing one instance from another one
+To get an access of an instance we have to keep that instance in a veriable. 
+Then we can call theat instance by using that veriables.
+*/
+
 const i1 = new Vue({
     el: "#i1",
     data: {
@@ -18,3 +21,21 @@ const i2 = new Vue({
         value: '2nd'
     },
 });
+
+const temp =`<p>My name is {{name}}</p>`;
+
+const i3 = new Vue({
+    // el: "#i3", //unmounted this instace from its element div
+    data: {
+        name: 'Nayeem'
+    },
+    //template: `<p>My name is {{name}}</p>`, 
+    //This is a new property where we can write our html using Backtag(``). 
+    template: temp,
+    //We can also use a variable to write our html if its too large. Then just simpally call the variable.
+});
+
+setTimeout(()=>{
+    i3.$mount('#i3');
+},3000)
+
